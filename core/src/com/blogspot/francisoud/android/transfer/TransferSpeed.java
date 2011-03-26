2c1175a14e7d84a5126177c04b68f611d71cd82f
Benjamin Francisoud <benjamin.francisoud@gmail.com>
2011-03-26 14:33:24 +0100

    format code with 180 char line wrap

--- core/src/com/blogspot/francisoud/android/transfer/TransferSpeed.java
+++ core/src/com/blogspot/francisoud/android/transfer/TransferSpeed.java
@@ -36,14 +36,12 @@ public class TransferSpeed extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		unitAdapter = new UnitAdapter(this,
-				android.R.layout.simple_spinner_item);
+		unitAdapter = new UnitAdapter(this, android.R.layout.simple_spinner_item);
 		devicesAdapter = new DevicesAdapter(this);
 
 		final Spinner unitSpinner = (Spinner) findViewById(R.id.unitSpinner);
 		unitSpinner.setSelected(false);
-		unitAdapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		unitSpinner.setAdapter(unitAdapter);
 		// select Kio/s
 		// unitSpinner.setSelection(3);
@@ -62,30 +60,26 @@ public class TransferSpeed extends Activity {
 		// });
 	}
 
-	private final class OnUnitSelectedListener implements
-			OnItemSelectedListener {
+	private final class OnUnitSelectedListener implements OnItemSelectedListener {
 		@Override
-		public void onItemSelected(AdapterView<?> parent, View view,
-				int position, long id) {
-			final String value = ((EditText) findViewById(R.id.editSpeedText))
-					.getText().toString();
+		public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+			final String value = ((EditText) findViewById(R.id.editSpeedText)).getText().toString();
 			if (Log.isLoggable(TAG, Log.DEBUG)) {
 				Log.d(TAG, "current value: " + value);
 				Log.d(TAG, "position: " + position);
 				Log.d(TAG, "id: " + id);
 			}
 			final AvailableUnit unit = unitAdapter.getUnit(position);
-			final double bits = Converter.toBit(new Double(value),
-					unit.getPower(), unit.getUnit());
+			final double bits = Converter.toBit(new Double(value), unit.getPower(), unit.getUnit());
 			Log.d(TAG, "bits: " + bits);
 			final String displayBits = formatter.format(bits);
 			// final String displayBits = Double.toString(bits);
-			Toast.makeText(getApplicationContext(), displayBits + " Bit/s",
-					Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(), displayBits + " Bit/s", Toast.LENGTH_SHORT).show();
 			final List<Device> devices = new ArrayList<Device>(Device.getList());
 			// FIXME use externalize string R.string.you;
 			final String label = "You are here";
-			devices.add(new Device(label, bits));
+			final Device here = new Device(label, bits);
+			devices.add(here);
 			Collections.sort(devices);
 			devicesAdapter.setNotifyOnChange(false);
 			devicesAdapter.clear();
