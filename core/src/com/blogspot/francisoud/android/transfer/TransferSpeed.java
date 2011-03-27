a058acbc04146c5fe3a9c759dc62fb0c97d5af58
Benjamin Francisoud <benjamin.francisoud@gmail.com>
2011-03-27 17:20:27 +0200

    put listener on ok button instead of unit spinner/selection

--- core/src/com/blogspot/francisoud/android/transfer/TransferSpeed.java
+++ core/src/com/blogspot/francisoud/android/transfer/TransferSpeed.java
@@ -8,13 +8,12 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
-import android.widget.Toast;
 
 public class TransferSpeed extends Activity {
 
@@ -33,39 +32,32 @@ public class TransferSpeed extends Activity {
 		unitAdapter = new UnitAdapter(this, android.R.layout.simple_spinner_item);
 		devicesAdapter = new ArrayAdapter<Device>(this, R.layout.device);
 
+		// unit selection
 		final Spinner unitSpinner = (Spinner) findViewById(R.id.unitSpinner);
 		unitAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		unitSpinner.setAdapter(unitAdapter);
-		unitSpinner.setOnItemSelectedListener(new OnUnitSelectedListener());
 
+		// ok button
+		final Button okButton = (Button) findViewById(R.id.okButton);
+		okButton.setOnClickListener(new OkButtonListener());
+
+		// devices list
 		final ListView listView = (ListView) findViewById(R.id.listView);
 		listView.setAdapter(devicesAdapter);
-		// listView.setOnItemClickListener(new OnItemClickListener() {
-		// public void onItemClick(AdapterView<?> parent, View view,
-		// int position, long id) {
-		// // When clicked, show a toast with the TextView text
-		// final CharSequence text = ((TextView) view).getText();
-		// Toast.makeText(getApplicationContext(), text,
-		// Toast.LENGTH_SHORT).show();
-		// }
-		// });
 	}
 
-	private final class OnUnitSelectedListener implements OnItemSelectedListener {
+	private final class OkButtonListener implements OnClickListener {
 		@Override
-		public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		public void onClick(View view) {
+			final Spinner unitSpinner = (Spinner) findViewById(R.id.unitSpinner);
 			final String value = ((EditText) findViewById(R.id.editSpeedText)).getText().toString();
 			if (Log.isLoggable(TAG, Log.DEBUG)) {
 				Log.d(TAG, "current value: " + value);
-				Log.d(TAG, "position: " + position);
-				Log.d(TAG, "id: " + id);
 			}
+			final int position = unitSpinner.getSelectedItemPosition();
 			final AvailableUnit unit = unitAdapter.getUnit(position);
 			final double bits = Converter.toBit(new Double(value), unit.getPower(), unit.getUnit());
 			Log.d(TAG, "bits: " + bits);
-			final String displayBits = FormatUtils.format(bits);
-			// final String displayBits = Double.toString(bits);
-			Toast.makeText(getApplicationContext(), displayBits + " Bit/s", Toast.LENGTH_SHORT).show();
 			final List<Device> devices = new ArrayList<Device>(Device.getList());
 			// FIXME use externalize string R.string.you;
 			final String label = "==> You are here";
@@ -80,11 +72,6 @@ public class TransferSpeed extends Activity {
 				devicesAdapter.add(device);
 			}
 			devicesAdapter.notifyDataSetChanged();
-		}
-
-		@Override
-		public void onNothingSelected(AdapterView<?> parent) {
-			Log.d(TAG, "onNothingSelected");
 		}
 	}
 }
